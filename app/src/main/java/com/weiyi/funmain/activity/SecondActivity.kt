623d495dc946f367d31a773f1cc80666e7d8ce97
tangwei <2278671454@qq.com>
2021-06-25 14:22:13 +0800

    需要解决嵌入后，setstate不更新text控件状态的问题

--- app/src/main/java/com/weiyi/funmain/activity/SecondActivity.kt
+++ app/src/main/java/com/weiyi/funmain/activity/SecondActivity.kt
@@ -1,10 +1,9 @@
 package com.weiyi.funmain.activity
 
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.learn.basemodule.config.AppMainSecond
 import com.weiyi.funmain.R
@@ -13,9 +12,14 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.dart.DartExecutor
 import kotlinx.android.synthetic.main.activity_second.*
 
-
+ /**
+  * @author:tw
+  * @date: 2021年6月25日10:38:41
+  * @email：2278671454@qq.com
+  * @description: flutter 模块页面承载页
+  */
 @Route(path = AppMainSecond)
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -31,6 +35,9 @@ class SecondActivity : AppCompatActivity() {
             ViewGroup.LayoutParams.MATCH_PARENT
         )
         val flutterEngine = FlutterEngine(this)
+
+        val initialRoute = "route1?{\"message\":\"向flutter传递初始数据\"}"
+        flutterEngine.navigationChannel.setInitialRoute(initialRoute)
         flutterEngine.dartExecutor.executeDartEntrypoint(
             DartExecutor.DartEntrypoint.createDefault()
         )
